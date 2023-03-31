0f0aa0515821e42af21094d0c154a77a62862158
landry.z <landry.ziyouma@gmail.com>
2023-03-31 16:58:08 +0100

    mail affectation de tache

--- mailing/views.py
+++ mailing/views.py
@@ -736,75 +736,101 @@ class affectationView(ViewSet):
     )
     def create(self, request):
 
-        try:
-            one_off = True
-            # destinator = request.data['destinator'] # 'email1', email2
-            object = request.data['object']
-            destinator = request.data['destinator']
-            
-            path = "mail/tasks/affectation.html"
-            path_txt = "mail/tasks/assignment.txt"
+        # try:
+        one_off = True
+        # destinator = request.data['destinator'] # 'email1', email2
+        object = request.data['object']
+        destinator = request.data['destinator']
+        
+        path = "mail/tasks/affectation.html"
+        path_txt = "mail/tasks/assignment.txt"
 
-            ctext = request.data['context']
-            context = {
-                "task": ctext['task'],
-                "project": ctext['project'],
-                "collaborateurs": ctext['collaborateurs'],
-                "description": ctext['description'],
-                "begin": ctext['begin'],
-                "end": ctext['end'],
-                "site_url": ctext['site_url'],
-                "doc_name": ctext['doc_name'],
-                "doc_link": ctext['doc_link'],
-            }
-            
-            html_content = render_to_string(
-                path,
-                context
-            )
-            
-            text_content = render_to_string(
-                path_txt,
-                context
-            )
-            
-            schedule, created = IntervalSchedule.objects.get_or_create(
-                every=30,
-                # period=unitime,
-                period=IntervalSchedule.SECONDS,
-            )
-            PeriodicTask.objects.update_or_create(
-                task="mailing.utils.send_mail",
-                # name="send_email",
-                name = uuid.uuid4().hex[:10].lower(),
-                args=json.dumps([destinator, object, text_content, html_content]),
-                
-                one_off = one_off,
-                start_time = datetime.now(),
-                defaults=dict(
-                    interval=schedule,
-                    expire_seconds=60,
-                ),
-            )
+        ctext = request.data['context']
 
-            
-            return Response(
-                {
-                    'message': 'New schedule is succefull run',
-                    'status': 'success',
-                    'code': status.HTTP_201_CREATED,
-                },
-                status=status.HTTP_201_CREATED)
+        date_end =  ctext['end']
+        date_end = date_end.replace("T", " ").split("+",1)[0]
+        end = datetime.strptime(date_end, '%Y-%m-%d %H:%M:%S')
+        end = end.strftime("%b %d %Y %H:%M:%S")
 
-        except Exception as e:
-            return Response(
-                {
-                    'message': 'Bad parameters',
-                    'status': 'Failed',
-                    'error': str(e),
-                    'code': status.HTTP_400_BAD_REQUEST,
-                },
-                status=status.HTTP_400_BAD_REQUEST)
+        date_begin =  ctext['begin']
+        date_begin = date_begin.replace("T", " ").split("+",1)[0]
+        begin = datetime.strptime(date_begin, '%Y-%m-%d %H:%M:%S')
+        begin = begin.strftime("%b %d %Y %H:%M:%S")
+        
+
+        task_created_at = ctext['task_created_at']
+        task_created_at = task_created_at.replace("T", " ").split("+",1)[0]
+        created_at = datetime.strptime(task_created_at, '%Y-%m-%d %H:%M:%S')
+        created_at = created_at.strftime("%b %d %Y %H:%M:%S")
+
+        task_update_at = ctext['task_update_at']
+        task_update_at = task_update_at.replace("T", " ").split("+",1)[0]
+        update_at = datetime.strptime(task_update_at, '%Y-%m-%d %H:%M:%S')
+        update_at = update_at.strftime("%b %d %Y %H:%M:%S")
+
+
+        context = {
+            "task": ctext['task'],
+            "project": ctext['project'],
+            "collaborateurs": ctext['collaborateurs'],
+            "description": ctext['description'],
+            "begin": begin,
+            "end": end,
+            "site_url": ctext['site_url'],
+            "doc_name": ctext['doc_name'],
+            "doc_link": ctext['doc_link'],
+            "task_created_at": created_at,
+            "task_update_at": update_at,
+            "company": ctext['company'],
+        }
+        
+        html_content = render_to_string(
+            path,
+            context
+        )
+        
+        text_content = render_to_string(
+            path_txt,
+            context
+        )
+        
+        schedule, created = IntervalSchedule.objects.get_or_create(
+            every=30,
+            # period=unitime,
+            period=IntervalSchedule.SECONDS,
+        )
+        PeriodicTask.objects.update_or_create(
+            task="mailing.utils.send_mail",
+            # name="send_email",
+            name = uuid.uuid4().hex[:10].lower(),
+            args=json.dumps([destinator, object, text_content, html_content]),
+            
+            one_off = one_off,
+            start_time = datetime.now(),
+            defaults=dict(
+                interval=schedule,
+                expire_seconds=60,
+            ),
+        )
+
+        
+        return Response(
+            {
+                'message': 'New schedule is succefull run',
+                'status': 'success',
+                'code': status.HTTP_201_CREATED,
+            },
+            status=status.HTTP_201_CREATED)
+
+        # except Exception as e:
+        #     return Response(
+        #         {
+        #             'message': 'Bad parameters',
+        #             'status': 'Failed',
+        #             'error': str(e),
+        #             'code': status.HTTP_400_BAD_REQUEST,
+        #         },
+        #         status=status.HTTP_400_BAD_REQUEST)
 
 
 
