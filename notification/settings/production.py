89b55ae3c9b6a15b3394d21a9f277164b5f5a9da
Dikongue Samuel <devKlivar@gmail.com>
2023-05-05 12:41:46 +0100

    dockerisatio celery

--- notification/settings/production.py
+++ notification/settings/production.py
@@ -7,7 +7,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY",
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["*"])
 ADMIN_URL = env("DJANGO_ADMIN_URL")
 
-DATABASES = {"default": env.db("DATABASE_URL")}
+ 
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
@@ -52,3 +52,34 @@ EMAIL_HOST_PASSWORD = env("SMTP_MAILGUN_PASSWORD")
 EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_USE_TLS = True
 DOMAIN = env("DOMAIN")
+
+
+CELERY_TIMEZONE ="Africa/Douala"
+ 
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_CACHE_BACKEND = 'django-cache'
+CELERY_TASK_TRACK_STARTED = True
+
+CELERY_RESULT_BACKEND_DB = f'db+mysql+pymysql://root:ziyouma@db/pseudo_mysql'
+# CELERY_RESULT_BACKEND_DB = os.getenv(
+#     "CELERY_RESULT_BACKEND_DB", f'db+mysql+pymysql://root:ziyouma@db/pseudo_mysql')
+CELERY_BROKER_URL = f'amqp://root:ziyouma@rabbitmq_mail//'
+# CELERY_BROKER_URL = f'amqp://root:ziyouma@rabbitmq//'
+CELERY_TASK_RESULT_EXPIRES = 18000
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+CELERY_IMPORTS = ["mailing.tasks"]
+
+EMAIL_HOST = os.getenv(
+    "EMAIL_HOST", 'smtp.gmail.com')
+EMAIL_PORT = os.getenv(
+    "EMAIL_PORT", 587)
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = os.getenv(
+    "EMAIL_HOST_USER", 'avotreecoute@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv(
+    "EMAIL_HOST_PASSWORD", '!Klivardev1')
+APP_NAME = 'Klivar'
