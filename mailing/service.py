61ee4ca36ad886ff5a190669926b77e3599eeb84
Dikongue Samuel <samyfabiol@gmail.com>
2025-08-12 11:28:53 +0100

    Verbose

--- mailing/service.py
+++ mailing/service.py
@@ -15,6 +15,7 @@ from service.utils import format_date_string_short, send_mail_created
  
 def mail_notification_start_projet_service(  name, dest_email, title, start_date, end_date, company,  description, url, back_url=None, lang=None ):
     # object and description
+
     if lang == "fr-FR" :
         path = "notification/audit_project/index-start-project-fr.html" 
         path_txt = "notification/audit_project/index-start-project-fr.txt" 
@@ -22,13 +23,13 @@ def mail_notification_start_projet_service(  name, dest_email, title, start_date
     elif lang == "en-US":
         path = "notification/audit_project/index-start-project-en.html" 
         path_txt = "notification/audit_project/index-start-project-en.txt" 
-        object = f"Audit launched: {title} ✅ Audit lancé : {title} ({format_date_string_short(start_date, lang)} at {format_date_string_short(end_date, lang)})"
+        object = f" ✅ Audit launched: {title} ({format_date_string_short(start_date, lang)} at {format_date_string_short(end_date, lang)})"
     else:
         path = "notification/audit_project/index-start-project-fr.html"
         path_txt = "notification/audit_project/index-start-project-fr.txt"  
         object = f"✅ Audit lancé : {title} ({format_date_string_short(start_date) } au {format_date_string_short(end_date)})"
     
-   
+
     context = {
         "name":name,  
         "start_date": format_date_string_short(start_date, lang),
