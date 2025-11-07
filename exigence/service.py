7a57f31468e2e6f6763f44ad06585433156e837c
Dikongue Samuel <samyfabiol@gmail.com>
2025-11-07 15:00:07 +0100

    add token to actor

--- exigence/service.py
+++ exigence/service.py
@@ -518,6 +518,7 @@ def follow_up_task( id_project, id_action,role,  id_client,  dest_email, full_na
                 logger.info(f"⏳ Pas encore de réponse.") 
                 # get data to  ExigenceMail 
                 ins_exigence = ExigenceMail.objects.filter( id_project=id_project, id_action=id_action, id_client=id_client).first()
+                logger.info(f"ins_exigence {ins_exigence} ")
                 if ins_exigence:
                     logger.info(f"⏳ Pas encore de réponse. {role} - {dest_email} ")
                     if role == "Responsable":
